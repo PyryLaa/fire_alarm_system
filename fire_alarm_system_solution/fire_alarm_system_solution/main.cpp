81a78ca516686690de0e340a29e460c5617f4549
Mika Miettinen <mika.olavi.miettinen@gmail.com>
2024-11-27 14:49:34 +0200

    Skeleton for project with ignores.

---
+++ fire_alarm_system_solution/fire_alarm_system_solution/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * fire_alarm_system_solution.cpp
+ *
+ * Created: 27.11.2024 14.45.37
+ * Author : Mika
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
